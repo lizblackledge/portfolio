32ad63261145a839be0b0ff8ef4d208620761952
Liz <Liz5112@users.noreply.github.com>
2022-06-25 17:56:27 -0500

    Create React Router Routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,76 @@
-import './styles.css';
+import { Routes, Route, Link } from "react-router-dom";
+import { Typography } from '@mui/material';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Liz Blackledge</h1>
-      </header>
+    <div className="App" >
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="portfolio" element={<Portfolio />} />
+        <Route path="contact" element={<Contact />} />
+      </Routes>
     </div>
+  );
+}
+
+function Home() {
+  return (
+    <>
+      <main>
+        <Typography>Hello from Home</Typography>
+      </main>
+      <nav>
+      <Link to="/about">About</Link>
+      <Link to="/portfolio">Portfolio</Link>
+      <Link to="/contact">Contact</Link>
+      </nav>
+    </>
+  );
+}
+
+function About() {
+  return (
+    <>
+      <main>
+        <Typography>Hello from About</Typography>
+      </main>
+      <nav>
+      <Link to="/">Home</Link>
+      <Link to="/portfolio">Portfolio</Link>
+      <Link to="/contact">Contact</Link>
+      </nav>
+    </>
+  );
+}
+
+function Portfolio() {
+  return (
+    <>
+      <main>
+        <Typography>Hello from Portfolio</Typography>
+      </main>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
+      </nav>
+    </>
+  );
+}
+
+function Contact() {
+  return (
+    <>
+      <main>
+        <Typography>Hello from Contact</Typography>
+      </main>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/portfolio">Portfolio</Link>
+      </nav>
+    </>
   );
 }
 
