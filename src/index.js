47284afc4573b945ea4be5f4d0bc96f2be7f4d01
Liz <Liz5112@users.noreply.github.com>
2022-06-25 17:55:23 -0500

    Set Up React BrowserRouter and Material UI Theme

--- src/index.js
+++ src/index.js
@@ -1,13 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './styles.css';
 import App from './App';
+import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider, createTheme } from '@mui/material';
 
+const theme = createTheme({
+  palette: {
+    primary: {
+      dark: '#008094',
+      main: '#00b8d4',
+      light: '#33c6dc',
+    },
+    secondary: {
+      dark: '#618833',
+      main: '#8bc34a',
+      light: '#a2cf6e',
+    }
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  
+  <ThemeProvider theme={theme}>
     <React.StrictMode>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </React.StrictMode>
+  </ThemeProvider>
 );
