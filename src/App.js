ee07ef8e7d16baba79c48b343ce7d51b039e9e93
Liz <Liz5112@users.noreply.github.com>
2022-06-25 14:42:28 -0500

    First Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './styles.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Liz Blackledge</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
