8ec347d7d5b178579fa7de8c824884ec5f1125a9
Liz <Liz5112@users.noreply.github.com>
2022-06-28 16:46:45 -0500

    Create Contact Form

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,12 +1,56 @@
-import { Link } from "react-router-dom";
-import { Typography, Container } from '@mui/material';
+// import { useState } from 'react';
+import theme from '../index';
+import { Typography, Container, TextField, Grid, Button } from '@mui/material';
 
-function Contact() {
+function Contact(props) {
   return (
-    <Container>
-      <Typography variant="h1" color="primary.dark">
-        Hi from Contact
-      </Typography>
+    <Container maxWidth="sm" direction={"column"}>
+      <Grid item>
+        <Typography variant="h3" color="primary.dark">
+          Let's get in touch~
+        </Typography>
+      </Grid>
+      <Grid item>
+        <TextField
+          id="outlined-name"
+          label="Name"
+          color="secondary"
+          margin="normal"
+          style={{ width: '48%', marginRight: '1rem' }}
+          // value={name}
+          // onChange={handleChange}
+          required
+        />
+      <TextField
+        id="outlined-email"
+        label="Email"
+        color="secondary"
+        margin="normal"
+        style ={{ width: '48.5%' }}
+        // value={email}
+        // onChange={handleChange}
+        required
+      />
+      </Grid>
+      <TextField
+        id="outlined-message"
+        label="Message"
+        color="secondary"
+        // margin="normal"
+        style={{ marginTop: 10 }}
+        fullWidth="true"
+        multiline
+        rows={5}
+        maxRows={15}
+        flex-basis="auto"
+        // value={message}
+        // onChange={handleChange}
+        required
+      />
+      <Grid item>
+        <Button variant="outlined" style={{ color: theme.palette.primary.dark, border: theme.palette.primary.dark, marginTop: 10 }} >Submit</Button>
+        {/* <Button variant="outlined" style={{ marginTop: 10 }}>Submit</Button> */}
+      </Grid>
     </Container>
   );
 }
