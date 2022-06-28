6f7d821b47407961e84010e3a734b7c6972406fb
Liz <Liz5112@users.noreply.github.com>
2022-06-28 16:41:20 -0500

    Add Navigate element to reroute to home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,23 @@
+import { Routes, Route, Navigate } from "react-router-dom";
 import './styles.css';
-import { Routes, Route } from "react-router-dom";
 import Navbar from './Navbar';
 import Home from './Pages/Home';
 import About from './Pages/About';
 import Portfolio from './Pages/Portfolio';
 import Contact from './Pages/Contact';
-// import { Typography } from '@mui/material';
 import theme from './index';
-// import background from './assets/shahadat-rahman.jpg'
 
-export function App(props) {
+function App(props) {
   return (
     <div className="App" theme={theme}>
-    {/* <div className="App" style={{backgroundImage: `url(${background})`}}> */}
       <div>
         <Navbar className="nav" theme={theme} />
         <Routes>
-          {/* <ul> */}
           <Route path="/" element={<Home />} />
           <Route path="about" element={<About />} />
           <Route path="portfolio" element={<Portfolio />} />
-          <Route path="contact" element={<Contact />} />
-          {/* <Route path="*" element={<Navigate to="/" />} /> */}
-          {/* </ul>  */}
+          <Route path="contact" element={<Contact theme={theme}/>} />
+          <Route path="*" element={<Navigate to="/" />} />
         </Routes>
       </div>
     </div>
