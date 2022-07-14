8548f67fd9dd8086bddca6412d4fcec6d3ca3201
Liz <Liz5112@users.noreply.github.com>
2022-07-13 21:42:46 -0500

    Update Card Information

--- src/Pages/Portfolio.jsx
+++ src/Pages/Portfolio.jsx
@@ -1,11 +1,10 @@
 // import { useState } from 'react';
 import { Typography, Container, Grid } from '@mui/material';
 import { Card, CardActions, CardContent, CardMedia, Button } from '@mui/material/';
-// import { } from '@mui/material';
-import coffeeShipping from '../assets/coffeeshipping.png';
-import langchat from '../assets/langchat.jpg';
+import estrella from '../assets/estrella.png';
 import kayakpal from '../assets/kayakpal.png';
-import drmario from '../assets/drmario.jpg';
+import langchat from '../assets/langchat.jpg';
+import kaffia from '../assets/kaffia.png';
 
 
 function Portfolio() {
@@ -15,26 +14,26 @@ function Portfolio() {
         My Recent Work~
       </Typography>
       <br></br>
-      <Grid container spacing={7} className="portfolio" direction={"row"} >
+      <Grid container  spacing={7} className="portfolio" direction={"row"} >
         <Grid item xs={6}>
           <Card className="portfolioCard" sx={{ maxWidth: 345 }}>
           <CardMedia
             component="img"
             height="140"
-            image={coffeeShipping}
+            image={estrella}
             alt="coffee shipping site"
           />
           <CardContent>
             <Typography gutterBottom variant="h5" component="div">
-              Wanderlust Coffee
+              Estrella Coffee Co. - E-Commerce Site for Artisan Coffee
             </Typography>
             <Typography variant="body2" color="text.secondary">
               Utilized React, Node.js, PostgreSQL, Authentication, Cookies, and Testing to create an exquisite site for dropshipping Artisan coffees from around the World.
             </Typography>
             </CardContent>
             <CardActions>
-              <Button size="small" href="https://github.com/Liz5112/wanderlustcoffee">Code</Button>
-              <Button size="small">View Site</Button>
+              <Button size="small" href="https://github.com/Liz5112/wanderlustcoffee" rel="noreferrer" target="_blank">Code</Button>
+              <Button size="small" href="https://estrellacoffeeco.github.io" rel="noreferrer" target="_blank">View Site</Button>
             </CardActions>
             </Card>
           </Grid>
@@ -48,15 +47,15 @@ function Portfolio() {
             />
             <CardContent>
               <Typography gutterBottom variant="h5" component="div">
-                LangChat
+                LangChat - Social Networking for Language Learners (In progress)
               </Typography>
               <Typography variant="body2" color="text.secondary">
               Utilized React, Node.js, PostgreSQL, Authentication, Cookies, and Testing to create an exquisite site for dropshipping Artisan coffees from around the World.
               </Typography>
             </CardContent>
             <CardActions>
-              <Button size="small" href="https://github.com/Liz5112/langchat">Code</Button>
-              <Button size="small">View Site</Button>
+              <Button size="small" href="https://github.com/Liz5112/langchat" rel="noreferrer" target="_blank">Code</Button>
+              <Button size="small" href="https://langchat.github.io" rel="noreferrer" target="_blank">View Site</Button>
             </CardActions>
             </Card>
           </Grid>
@@ -70,15 +69,15 @@ function Portfolio() {
             />
             <CardContent>
               <Typography gutterBottom variant="h5" component="div">
-                Kayak Pal
+                Kayak Pal - Find Places to Kayak and See Whether the Weather is Suitable for Kayaking Today
               </Typography>
               <Typography variant="body2" color="text.secondary">
               Utilized React, Node.js, PostgreSQL, Authentication, Cookies, and Testing to create an exquisite site for dropshipping Artisan coffees from around the World.
               </Typography>
               </CardContent>
               <CardActions>
-                <Button size="small" href="https://github.com/Liz5112/kayakpal">Code</Button>
-                <Button size="small">View Site</Button>
+                <Button size="small" href="https://github.com/Liz5112/kayak-pal" rel="noreferrer" target="_blank">Code</Button>
+                <Button size="small" href="https://kayak-pal.github.io" rel="noreferrer" target="_blank">View Site</Button>
               </CardActions>
             </Card>
           </Grid>
@@ -86,21 +85,21 @@ function Portfolio() {
             <Card className="portfolioCard" sx={{ maxWidth: 345 }}>
             <CardMedia
               component="img"
-              height="140"
-              image={drmario}
+              height="240"
+              image={kaffia}
               alt="doctor mario game"
             />
             <CardContent>
               <Typography gutterBottom variant="h5" component="div">
-                Dr. Mario Emmulator
+                Apache Kafka Visualizer and Administrator
               </Typography>
               <Typography variant="body2" color="text.secondary">
-              Utilized React, Node.js, PostgreSQL, Authentication, Cookies, and Testing to create an exquisite site for dropshipping Artisan coffees from around the World.
+              Open-Source Dev Tool created to help Kafka engineers spin up clusters and monitor their cluster health efficiently in an Electron desktop application GUI.
               </Typography>
             </CardContent>
             <CardActions>
-              <Button size="small" href="https://github.com/Liz5112/drmario">Code</Button>
-              <Button size="small">View Site</Button>
+              <Button size="small" href="https://github.com/Liz5112/kaffia" rel="noreferrer" target="_blank">Code</Button>
+              <Button size="small" href="https://kaffia.dev" rel="noreferrer" target="_blank">View Site</Button>
             </CardActions>
           </Card>
         </Grid>
