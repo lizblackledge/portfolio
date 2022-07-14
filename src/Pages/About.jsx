ccb28ef11153119b3c50a78bd9a370324ad51c89
Liz <Liz5112@users.noreply.github.com>
2022-07-13 21:43:44 -0500

    Update About Content, add Image, and Link to Open Source

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,9 +1,23 @@
-import { Typography, Container } from '@mui/material';
+import { Typography, Container, Box } from '@mui/material';
+import code from '../assets/shahadat-rahman-BfrQnKBulYQ-unsplash.jpg'
 
 function About() {
   return (
-    <Container className="container" maxWidth="md">
-      <Typography variant="h4" color="primary.dark" className="content">My name is Liz Blackledge and I love to code. Oh, how I love it. It's great. I enjoy new challenges and creating new things. Oh, I'm so great and interesting. You really want me on your team! Oh, how I love computers! I have been coding since I was 7 and built my own computer when I was 12. Apart from coding, I enjoy learning about new cultures and languages. Kayaking and hiking are the best. I have studied a little martial arts in the past, and I want to get back into it soon.</Typography>
+    <Container className="container" maxWidth="large" sx={{ display: "flex", align: "center" }}>
+      <Box
+          component="img"
+          maxWidth="35%"
+          marginLeft="1rem"
+          boxShadow={5}
+          alt="computer code"
+          src={code}
+        />
+      <Container>
+        <Typography variant="h6" color="primary.dark" margin="1rem" className="content">My name is Liz Blackledge and I enjoy solving complex problems and building cool things.</Typography>
+        <Typography variant="h6" color="primary.dark" margin="1rem" className="content">The bulk of my experience is in React(Router and Hooks), Node.js/Express, PostgreSQL, and MongoDB, but I'm always keen on tackling new challenges and learning new things.</Typography>
+        <Typography variant="h6" color="primary.dark" margin="1rem" className="content">Earlier this year, my team built an Open-Source <a href="https://kaffia.dev" rel="noreferrer" target="_blank">Apache Kafka Visualizer and Administrator</a> that containerizes and deploys Kafka clusters in Docker, imports cluster health metrics using Prometheus, and utilizes Grafana to display them in an Electron desktop application.</Typography>
+        <Typography variant="h6" color="primary.dark" margin="1rem" className="content">Most recently, I have been focusing on implementing testing with Jest and React Testing Library, improving User Authentication with OAuth and Bcrypt, and  learning Typescript.</Typography>
+      </Container>
     </Container>
   );
 }
