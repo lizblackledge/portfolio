d65517fabd4a1c4595bf24d78a9b625b65f937e8
Liz <Liz5112@users.noreply.github.com>
2022-07-13 21:44:33 -0500

    Fix hover styling

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -14,12 +14,17 @@ function Navbar(props) {
     <nav>
       <ul className="NavbarItems" sx={{display: 'inline-flex' }}>
         <Link to="/" className="title" ><Typography variant="h5" color="secondary.dark">Liz Blackledge</Typography></Link>
-        <Link to="/about" className="links"><Typography variant="h6" color="secondary.dark">About</Typography></Link>
-        <Link to="/portfolio" className="links"><Typography variant="h6" color="secondary.dark">Portfolio</Typography></Link>
-        <Link to="/contact" className="links" ><Typography variant="h6" color="secondary.dark">Contact</Typography></Link>
-        <FaGithub className="links" size={35} style={{ color: primary }} />
-        <FaLinkedin className="links" size={35} style={{ color: primary }} />
-
+        <Link to="/about" className="pages"><Typography variant="h6" color="secondary.dark" className="pages" >About</Typography></Link>
+        <Link to="/portfolio" className="pages"><Typography variant="h6" color="secondary.dark" className="pages" >Portfolio</Typography></Link>
+        <Link to="/contact" className="pages" ><Typography variant="h6" color="secondary.dark" className="pages">Contact</Typography></Link>
+        <div className="links">
+          <a href="https://github.com/Liz5112" rel="noreferrer" target="_blank">
+            <FaGithub className="links" size={35} style={{ color: primary }} />
+          </a>
+          <a href="https://www.linkedin.com/in/lizblackledge01/" rel="noreferrer" target="_blank">
+            <FaLinkedin className="links" size={35} style={{ color: primary }} />
+          </a>
+        </div>
       </ul>
     </nav>
   )
